98764ec7c288bcad3c351c7de60fdcc565e01276
jcartronics <jcar-95@hotmail.com>
2023-09-19 19:06:55 -0300

    feat:(Post) Agrega validaciones a Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,27 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
   has_many_attached :images
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validate :image_content_type
+  validate :image_file_size
+
+  def image_content_type
+    images.each do |image|
+      puts image.content_type
+      if !image.content_type.in?(%('image/jpeg image/png'))
+        errors.add(:images, "La imagen debe ser JPEG o PNG")
+        puts "La imagen debe ser JPEG o PNG y es #{image.content_type}"
+      end
+    end
+  end
+
+  def image_file_size
+    images.each do |image|
+      if image.byte_size > 5.megabytes
+        errors.add(:images, "La imagen debe ser menor que 5MB")
+      end
+    end
+  end
 end
