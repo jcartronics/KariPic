876202cb6c3c66619c1a744421ebe20c086650cb
jcartronics <jcar-95@hotmail.com>
2023-09-19 19:19:07 -0300

    feat:(User) Agrega validaciones

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,24 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_one_attached :avatar
 
+  validates :name, presence: true
+  validates :avatar, presence: true
+  validate :avatar_content_type
+  validate :avatar_file_size
+
+  def avatar_content_type
+    puts avatar.content_type
+    if !avatar.content_type.in?(%('image/jpeg image/png'))
+      errors.add(:avatar, "La imagen debe ser JPEG o PNG")
+      puts "La imagen debe ser JPEG o PNG y es #{avatar.content_type}"
+    end
+  end
+
+  def avatar_file_size
+    if avatar.byte_size > 5.megabytes
+      errors.add(:avatar, "La imagen debe ser menor que 5MB")
+    end
+  end
+
   enum role: %w[user admin]
 end
